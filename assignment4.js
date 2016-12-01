18d76367e496f78cb8589dd389206c22124b9e1d
Jennifer Bramson <jib41@pitt.edu>
2016-12-01 02:02:35 -0500

    Added autocomplete

--- assignment4.js
+++ assignment4.js
@@ -11,7 +11,59 @@
 // $.html(), $.text(), etc.
 // keyup events could be helpful to get value of field as the user types
 
+var param = ['interests', 'programming', 'comics'];
+
 (function() {
   // Magic!
   console.log('Keepin\'n it clean with an external script!');
+
+  // See if search is being written in
+  $('.flexsearch-input').on("input", function() {
+
+    // Delete any old matches displayed
+    $('#results').hide();
+    $('li').remove();
+
+    // Get data from the API
+    value = (this.value).toLowerCase();
+    if (value != "") {
+      for (var i = 0; i < param.length; i++) {
+        showMatches(i, value);
+      }
+
+    }
+  });
+
 })();
+
+
+function showMatches(i, input_data) {
+
+  var state = $.ajax({
+    url: 'http://www.mattbowytz.com/simple_api.json?data=' + param[i],
+    type: 'get',
+
+    // Success checks that readyState is 4 (success) and status is 200 (success)
+    success: function (result) {
+      // Look for data that starts with the user's input in the search bar
+      var returned = state.responseJSON.data;
+      for (var j = 0; j < returned.length; j++) {
+        var lc_data = returned[j].toLowerCase(); // lowercase data
+        if (lc_data.startsWith(input_data)) {
+          // Display by adding an li of class accordian to results
+          $('#results').show();
+          $('#results').append('<li class="accordian">' + lc_data + '<li>');
+        }
+      }
+    }
+  });
+
+}
+
+
+// Open clicked suggestion in new tab
+// Need to add the .accordian as they are created, so do from body
+$('body').on('click', '.accordian', function() {
+  var text = $(event.target).text();
+  window.open('https://www.google.com/#safe=active&q=' + text, '_blank');
+});
